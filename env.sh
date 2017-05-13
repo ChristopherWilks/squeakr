6c9168d46538b20926ede74c494493d618326b27
ChristopherWilks <broadsword@gmail.com>
2017-05-13 17:06:32 -0400

    removed call to clean_env.sh

--- env.sh
+++ env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 module load zlib/1.2.8
-source clean_env.sh
 export INCLUDE=
 export CPATH=/cm/shared/apps/boost/1.5.9/include:$CPATH
 export LIBRARY_PATH=/cm/shared/apps/boost/1.5.9/lib:/cm/shared/apps/zlib/1.2.8/lib:$LIBRARY_PATH
